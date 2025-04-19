42062541ab864be255eec56b006bd8e7b6f21529
dylan-turner25 <dylan.turner25@uga.edu>
2025-04-19 07:05:55 -0400

    adding plc payment rate dataset

--- R/data.R
+++ R/data.R
@@ -1,12 +1,10 @@
-#' Commodity specific marketing year average prices as published by the USDA Farm Service Agency.
-#'
-#' @description Marketing year average prices by commodity.
+#' Commodity specific marketing year average prices.
 #'
 #' @details
 #' To view code used to generate this data set, see `./data-raw/fsaArcPlc/supplementary_files/fsaMyaPrice.R`
 #'
 #'
-#' @format  A data frame
+#' @format  A tibble
 #' \describe{
 #'   \item{Commodity}{The name of the commodity}
 #'   \item{Marketing Year}{The dates for the commodity specific marketing year }
@@ -25,3 +23,28 @@
 #' @source \url{https://www.fsa.usda.gov/resources/programs/arc-plc/program-data}
 #'
 "fsaMyaPrice"
+
+#' Commodity specific PLC payment rates including statutory and effective reference prices.
+#'
+#' @details
+#' To view code used to generate this data set, see `./data-raw/fsaArcPlc/supplementary_files/fsaPlcPaymentRates.R`
+#'
+#'
+#' @format  A tibble
+#' \describe{
+#'   \item{Commodity}{The name of the commodity}
+#'   \item{Marketing Year}{The dates for the commodity specific marketing year }
+#'   \item{year}{The two calendar years that the commodity specific marketing year spans}
+#'   \item{Publishing Dates for the  Final T - 0 PLC Effective Price}{The date the final T-0 PLC effective price was published}
+#'   \item{Unit}{The unit of measurement for the price}
+#'   \item{Statutory Reference Price}{The statutory reference price for the commodity}
+#'   \item{Final T-0 Effective Price}{The effective reference price for the commodity in the marketing year defined by `year`}
+#'   \item{Final T-0 MYA Price}{The marketing year average price for the marketing year defined by `year`}
+#'   \item{T-0 National Loan Rate}{The national loan rate for the commodity in the marketing year defined by `year`}
+#'   \item{Final T-0 PLC Payment Rate}{The PLC payment rate for the commodity in the marketing year defined by `year`}
+#'   \item{Maximum PLC Payment Rate}{The maximum PLC payment rate for the commodity in the marketing year defined by `year`}
+#'   }
+#' @usage data(fsaPlcPaymentRate)
+#' @source \url{https://www.fsa.usda.gov/resources/programs/arc-plc/program-data}
+#'
+"fsaPlcPaymentRate"
