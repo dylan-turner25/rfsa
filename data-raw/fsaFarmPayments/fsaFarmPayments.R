e9bdd99512740749c5ac07d7aca0e05d3dad81bf
dylan-turner25 <dylan.turner3@outlook.com>
2025-05-22 07:35:36 -0400

    removing temporary code in source code for fsaFarmPayments

--- data-raw/fsaFarmPayments/fsaFarmPayments.R
+++ data-raw/fsaFarmPayments/fsaFarmPayments.R
@@ -42,46 +42,16 @@ piggyback::pb_new_release(
   body = "This release contains all individual year-program files."
 )
 
-# 2. upload your .rds files into that release
+#upload your .rds files into that release
 rds_files <- list.files("data-raw/fsaFarmPayments/output_data", "\\.rds$",
                         full.names = TRUE, recursive = TRUE)
 
-release <- gh::gh(
-  "/repos/{owner}/{repo}/releases/latest",
-  owner = "dylan-turner25",
-  repo  = "rfsa"
-)
-
-# 2. Extract the assets list
-assets <- release$assets
-
-# 3. Pull out the bits you care about
-df <- data.frame(
-  name = vapply(assets, `[[`, "", "name"),
-  url  = vapply(assets, `[[`, "", "browser_download_url"),
-  size = vapply(assets, `[[`, 0,  "size"),
-  stringsAsFactors = FALSE
-)
-
-df <- df %>% filter(size < 350)
-
-rds_files <- rds_files[which(basename(rds_files) %in% df$name)]
-
-# get file size of each rds_files
-rds_files <- rds_files[which(file.size(rds_files) > 309)]
-
-cache_files <- list.files(dest_dir, full.names = TRUE, recursive = TRUE)
-
-# delete any files in cache_files that are less than 350 bytles
-cache_files <- cache_files[which(file.size(cache_files) < 350 & grepl("2003|2004|2005", cache_files))]
-
-file.remove(cache_files)
 
 piggyback::pb_upload(
   rds_files,
   repo = "dylan-turner25/rfsa",
   tag  = "v0.1.0",
-  overwrite = T
+  overwrite = F
 )
 
 
